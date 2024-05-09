86b3158c5655b448d3429eacb40ad69bb97aae30
zicaacademy <138627459+zicaacademy@users.noreply.github.com>
2024-05-09 11:50:25 -0300

    Atividade 4

---
+++ Atividade4/Calculadora.js
@@ -0,0 +1,22 @@
+function somar(a, b) {
+    return Number(a) + Number(b);
+}
+
+function subtrair(a, b) {
+    return Number(a) - Number(b);
+}
+
+function multiplicar(a, b) {
+    return Number(a) * Number(b);
+}
+
+function dividir(a, b) {
+    return Number(a) / Number(b);
+}
+
+module.exports = {
+    somar,
+    subtrair,
+    multiplicar,
+    dividir
+};
